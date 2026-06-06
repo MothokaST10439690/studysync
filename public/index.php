5b8c3c85c6dcca9a574c1ccec75c86a88ea209d1
MothokaST10439690 <167367669+MothokaST10439690@users.noreply.github.com>
2026-06-06 22:58:34 +0200

    fix routing

--- public/index.php
+++ public/index.php
@@ -1,2 +1,16 @@
 <?php
-require dirname(__DIR__) . '/index.php';
+$request = $_SERVER['REQUEST_URI'];
+$file = dirname(__DIR__) . parse_url($request, PHP_URL_PATH);
+
+if (is_file($file)) {
+    return false;
+}
+
+$page = ltrim(parse_url($request, PHP_URL_PATH), '/');
+$target = dirname(__DIR__) . '/' . ($page ?: 'index.php');
+
+if (is_file($target)) {
+    require $target;
+} else {
+    require dirname(__DIR__) . '/index.php';
+}
